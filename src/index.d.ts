bef4cb04c48de064905b052cf89ab627c3ed2883
d0kur0 <dev.dokuro@gmail.com>
2021-08-05 01:48:14 +0300

    Играюсь с типами.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,5 @@
 declare module "@d0kur0/uikit" {
     import type { DefineComponent } from 'vue'
-    const Button: DefineComponent<{}, {}, any>
 
-    export { Button };
+    export const Button: DefineComponent<{ transparent: boolean }, {}, any>
 }
