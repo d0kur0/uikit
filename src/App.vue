b7d8674b5b9fe0911739db737062debd6b762ed9
d0kur0 <dev.dokuro@gmail.com>
2021-08-05 01:25:09 +0300

    Install empty app, add test component button

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Buttons</h1>
+
+  <div class="gap">
+    <Button>
+      Default button
+    </Button>
+
+    <Button :transparent="true">
+      Button with out background
+    </Button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Button from './components/Button.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Button
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: sans-serif;
+}
+
+.gap {
+  display: flex;
+  gap: 10px;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,49 @@
+<template>
+  <button :class="{'button-transparent': transparent}" class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Button',
+  props: {
+    transparent: Boolean
+  },
+});
+</script>
+
+<style scoped>
+.button {
+  font: inherit;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  background-color: #AD5CAD;
+  color: #fff;
+  border: 2px solid #AD5CAD;
+  border-radius: 7px;
+  padding: 8px 20px;
+  transition: 0.3s;
+  cursor: pointer;
+}
+
+.button:hover {
+  opacity: 0.9;
+}
+
+.button:active {
+  background-color: #b149b1;
+}
+
+.button-transparent {
+  background-color: transparent;
+  color: #AD5CAD;
+}
+
+.button-transparent:active {
+  background-color: transparent;
+  color: #b149b1;
+  border-color: #b149b1;
+}
+</style>
